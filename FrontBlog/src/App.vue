273650a842b8ddf45a0be8c83eb8267e4f3aee43
SuanLa <2444066058@qq.com>
2023-03-10 22:23:10 +0800

    增加所需包

--- FrontBlog/src/App.vue
+++ FrontBlog/src/App.vue
@@ -1,20 +1,17 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from "vue";
+const background = ref("black");
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="star logo" class="logo" src="./assets/img.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome zeroonestar!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
--- FrontBlog/src/main.js
+++ FrontBlog/src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 
 import './assets/main.css'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
 createApp(App).mount('#app')
