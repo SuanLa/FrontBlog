f323ad2714affe330fd888b4476be4fe297aa2cb
SuanLa <2444066058@qq.com>
2023-03-11 10:53:34 +0800

    test

--- FrontBlog/src/App.vue
+++ FrontBlog/src/App.vue
@@ -2,23 +2,38 @@
 import HelloWorld from './components/HelloWorld.vue'
 import {ref} from "vue";
 const background = ref("black");
+
 </script>
 
 <template>
+  <div class="main">
   <header>
     <img alt="star logo" class="logo" src="./assets/img.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Welcome zeroonestar!" />
+      <HelloWorld msg="操作系统"/>
+      <HelloWorld msg="数据结构与算法"/>
+      <HelloWorld msg="计算机网络"/>
+      <HelloWorld msg="计算机组成原理"/>
     </div>
   </header>
+  <main>
+    test
+  </main>
+  <aside>
+
+  </aside>
+  </div>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
 }
-
+.main{
+  background-color: red;
+}
 .logo {
   display: block;
   margin: 0 auto 2rem;
@@ -41,4 +56,5 @@ header {
     flex-wrap: wrap;
   }
 }
+
 </style>
